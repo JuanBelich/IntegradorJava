4d05306f241600be1dbd19d70437209f545f9157
Juan <127412987+JuanBelich@users.noreply.github.com>
2023-11-07 20:46:25 -0300

    Update palabraSecreta.java

--- Integrador/src/domain/palabraSecreta.java
+++ Integrador/src/domain/palabraSecreta.java
@@ -7,7 +7,7 @@ class palabraSecreta {
 //Metodo estático para la palabra secreta que devuelve la palabra secreta, es decir un string
     static String obtenerPalabraSecreta() {
         //Array de palabras 
-        String [] palabras = {"perro","tecnologica","mendoza","universida","arquitectura", "ovoviviparo", "caleidoscopio", "campo", "juega", "mina", "jazmin", "carnear", "limon", "bondadoso", "desquiciado", "eolico", "modernismo", "sindicato", "localmente", "barbacoa", "blanquecino", "armario", "decisivo", "teclado","proyecto","github","switch","herencia","polimorfismo","encapsulamiento","obstraccion"};
+        String [] palabras = {"perro","tecnologica","mendoza","universidad","arquitectura", "ovoviviparo", "caleidoscopio", "campo", "juega", "mina", "jazmin", "carnear", "limon", "bondadoso", "desquiciado", "eolico", "modernismo", "sindicato", "localmente", "barbacoa", "blanquecino", "armario", "decisivo", "teclado","proyecto","github","switch","herencia","polimorfismo","encapsulamiento","obstraccion"};
         //Usamos la clase random para elegir una palabra al azar 
         Random r = new Random(); //Hay que importar la clase Random
         //Esto generará un numero aleatorio (-1 a la cantidad de palabras que tenemos para que entre dentro del rango)
